3fa7563cda02d16bf8531119f7e0373530010290
Morgan Jones <morganmwj@gmail.com>
2024-10-27 17:44:54 +0000

    Add project files.

---
+++ Notes/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Notes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Register the route for NotePage
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
+    }
+}
